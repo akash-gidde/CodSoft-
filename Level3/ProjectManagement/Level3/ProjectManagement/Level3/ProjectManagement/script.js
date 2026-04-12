ee74690dcd83c6f365fe24d80719309628677489
akash-gidde <akashgidde996@gmail.com>
2026-04-12 18:39:02 +0530

    Added add and delete task functionality

--- Level3/ProjectManagement/Level3/ProjectManagement/Level3/ProjectManagement/script.js
+++ Level3/ProjectManagement/Level3/ProjectManagement/Level3/ProjectManagement/script.js
@@ -1,27 +1,24 @@
 function addTask() {
   let input = document.getElementById("taskInput");
-  let task = input.value;
+  let taskValue = input.value;
 
-  if (task === "") {
-    alert("Enter task");
+  if (taskValue === "") {
+    alert("Please enter a task");
     return;
   }
 
   let li = document.createElement("li");
-  li.innerHTML = `
-    ${task}
-    <button onclick="completeTask(this)">Done</button>
-    <button onclick="deleteTask(this)">Delete</button>
-  `;
+  li.innerText = taskValue;
 
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+
+  delBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(delBtn);
   document.getElementById("taskList").appendChild(li);
+
   input.value = "";
-}
-
-function completeTask(btn) {
-  btn.parentElement.style.textDecoration = "line-through";
-}
-
-function deleteTask(btn) {
-  btn.parentElement.remove();
 }
