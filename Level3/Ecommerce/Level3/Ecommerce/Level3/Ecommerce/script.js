eafff156a99454b18029891a0075af952f0a517e
akash-gidde <akashgidde996@gmail.com>
2026-04-12 18:34:39 +0530

    Added Ecommerce cart functionality
    Refactored addToCart function to use product names and added displayCart function to show cart items.

--- Level3/Ecommerce/Level3/Ecommerce/Level3/Ecommerce/script.js
+++ Level3/Ecommerce/Level3/Ecommerce/Level3/Ecommerce/script.js
@@ -1,6 +1,17 @@
-let total = 0;
+let cart = [];
 
-function addToCart(price) {
-  total += price;
-  document.getElementById("total").innerText = total;
+function addToCart(productName) {
+  cart.push(productName);
+  displayCart();
+}
+
+function displayCart() {
+  let cartBox = document.getElementById("cart");
+  cartBox.innerHTML = "";
+
+  cart.forEach(function(item) {
+    let p = document.createElement("p");
+    p.innerText = item;
+    cartBox.appendChild(p);
+  });
 }
